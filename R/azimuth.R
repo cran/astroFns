0a1f53c0fb54f54628d0b6ecaadca99fc113f2a2
Andrew Harris <harris@astro.umd.edu>
2022-05-08 16:20:02 +0000

    version 4.2-1

--- R/azimuth.R
+++ R/azimuth.R
@@ -5,7 +5,8 @@ function(dec.sou='33d 09m 35.0s', ha=0, lat.obs='38d 25m 59.2s') {
     # h is hour angle [dec.souimal hours]
     # lat.obs is observatory latitude [dms string]
     # "Astrophysical Formulae," K.R. Lang, c. 1986, 5-45
-    # AH 12/28/08, 2/22/10
+    # Also Astronomical Alamanac, B86
+    # AH 12/28/08, 2/22/10, fixed error 2021-10-16
 
     # Convert observatory latitude, source dec.sou and hour angle to radians
     dec.sou <- dms2rad(dec.sou)
@@ -14,7 +15,7 @@ function(dec.sou='33d 09m 35.0s', ha=0, lat.obs='38d 25m 59.2s') {
     # Calculate elevation and parts for azimuth
     a <- asin(sin(lat.obs)*sin(dec.sou)+cos(lat.obs)*cos(dec.sou)*cos(ha))
     az1 <- -cos(dec.sou)*sin(ha)/cos(a)
-    az2 <- (sin(dec.sou)*cos(lat.obs)-cos(lat.obs)*cos(dec.sou)*cos(ha))/cos(a)
+    az2 <- (sin(dec.sou)*cos(lat.obs)-sin(lat.obs)*cos(dec.sou)*cos(ha))/cos(a)
     # Then azimuth
     az <- atan2(az1, az2) * 180/pi
     idx <- which(az<0)
--- R/ut2dmjd.R
+++ R/ut2dmjd.R
@@ -2,12 +2,12 @@ ut2dmjd <-
 function(yr=2012, mo=1, dy=1, hr=0, mi=0, se=0) {
 # Convert from UT day and time to decimal modified Julian date
 #
-# A. Harris, U. Maryland Astronomy, 4/18/2008, 1/8/2009, 6/12/2012
+# A. Harris, U. Maryland Astronomy, 4/18/2008, 1/8/2009, 6/12/2012, 12/5/2021
 #
 # Uses function ymd2jd to compute Julian date
 
-    if (dy <= 0) stop('*** Day must be positive ***')
-    if (mo < 1 | mo > 12) stop('*** Month must be within a year ***')
+    if (any(dy <= 0)) stop('*** Day must be positive ***')
+    if (any(mo < 1) | any(mo > 12)) stop('*** Month must be within a year ***')
 
     ymd2jd(yr, mo, dy) + (hr + (mi + se/60)/60)/24 - 2400000.5
 
